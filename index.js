2d9d60d8fc1e8369e3312c9dca819635d463b2c3
swix <swix@swixs-MacBook-Pro.local>
2016-11-24 18:41:15 +0100

    Patched .

--- index.js
+++ index.js
@@ -30,17 +30,20 @@ const EXT = '.svg'
 const OUT = `${toTest}${EXT.substring(1)}.json`
 
 // Object helper
-const assocPath = (target, path, file) => {
+const assocPath = (obj, path, file) => {
   FILES ++
   let objPath = Path.dirname(getRelPath(path)).split(Path.sep)
-  let ref = target
-  objPath.forEach(key => {
-    if (!ref[key]) {
-      DIRS ++
-      ref[key] = {}
-    }
-    ref = ref[key]
-  })
+  let ref = obj
+
+  if (objPath[0] !== '.') {
+      objPath.forEach(key => {
+      if (!ref[key]) {
+        DIRS ++
+        ref[key] = {}
+      }
+      ref = ref[key]
+    })
+  }
   ref[Path.basename(file, EXT)] = Fs.readFileSync(path, 'UTF-8')
 }
 
@@ -48,18 +51,6 @@ nlog(
   `Running in ${Path.resolve('.')}`,
   `Mapping ${Path.resolve(testPath)} for ${EXT} files`
 )
-
-// const doDir = dirPath => {
-//   Fs.readdir(dirPath,
-//     (err, res) => {
-//       if (err) {
-//         log(err);
-//         return err
-//       } else if (res.length) {
-//         res.forEach(fileName => checkFile(Path.resolve(Path.join(dirPath, fileName))));
-//       }
-//     })
-// }
 
 const doDir = dirPath => {
   const res = Fs.readdirSync(dirPath)
@@ -80,29 +71,12 @@ const checkFile = filePath => {
     doDir(filePath)
   }
 }
-//
-// const checkFile = filePath => {
-//   Fs.stat(filePath,
-//     (err, res) => {
-//       if (err){
-//         log(err.message)
-//         return err
-//       } else if (res.isFile() && Path.extname(filePath) === EXT) {
-//         onMatch(filePath)
-//       } else if (res.isDirectory()) {
-//         doDir(filePath)
-//       }
-//     })
-// }
 
 const main = () => {
   doDir(toTest)
-  // setTimeout(() => {
-    log(`Found ${FILES} ${EXT} files in ${DIRS} directories`)
-    log(`Writing output to ${OUT}`)
-    Fs.writeFileSync(OUT, JSON.stringify(toJSON))
-  // }
-  // ,200)
+  log(`Found ${FILES} ${EXT} files in ${DIRS} directories`)
+  log(`Writing output to ${OUT}`)
+  Fs.writeFileSync(OUT, JSON.stringify(toJSON))
 }
 
 main()
